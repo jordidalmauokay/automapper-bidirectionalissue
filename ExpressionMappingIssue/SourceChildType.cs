318ea441a5bfe19e2ed31f36785980233fc050b4
Robert Te Kaat <RobertTeKaat@ireckonu.com>
2024-01-18 15:16:39 +0100

    Initial version

---
+++ ExpressionMappingIssue/SourceChildType.cs
@@ -0,0 +1,8 @@
+﻿namespace ExpressionMappingIssue
+{
+    internal class SourceChildType
+    {
+        public int Id { get; set; }
+        public IEnumerable<SourceListItemType> ItemList { get; set; } // Uses same type (SourceListItemType) for its itemlist as SourceType
+    }
+}
